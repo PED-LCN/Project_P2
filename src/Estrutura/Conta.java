2f982da11ff1baea84619a3322aacae6282fe6c4
Luiz451 <luiz.fernando.sf2006@gmail.com>
2025-10-13 12:10:24 -0300

    Criando atributos e métodos para as classes

--- src/Estrutura/Conta.java
+++ src/Estrutura/Conta.java
@@ -1,5 +1,18 @@
 package Estrutura;
 
 public class Conta {
+    private String agencia;
+    private String numero;
+    private double saldo;
+    private Cliente titular;
+
+    public Conta(Cliente titular, String agencia, String numero) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.titular = titular;
+    }
+    public Cliente getTitular() {
+        return titular;
+    }
 
 }
