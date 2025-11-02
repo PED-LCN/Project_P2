57a9a8cf7e6d8593055c371fbf01089d25779c11
Kaikyterto <kaikyterto9@gmail.com>
2025-11-02 11:38:12 -0300

    Update Pizza.java
    Definir os preços automaticamente com base no tamanho e sabor escolhido

--- src/estrutura/Pizza.java
+++ src/estrutura/Pizza.java
@@ -8,10 +8,10 @@ public class Pizza {
     private double preco;
     private ArrayList<String> ingredientesAdicionais;
 
-    public Pizza(String sabor, String tamanho, double preco) {
+    public Pizza(String sabor, String tamanho) {
         this.sabor = sabor;
         this.tamanho = tamanho;
-        this.preco = preco;
+        this.preco = definirPrecoBase(sabor);
     }
 
     public double getPreco() {
@@ -24,4 +24,49 @@ public class Pizza {
     public void setPreco(double preco) {
         this.preco = preco;
     }
+    private double precoBaseTamanho(String tamanho){
+        double precoBase = 20.0;
+        switch (tamanho.toLowerCase()) {
+            case "pequena":
+                precoBase *= 1; 
+                break;
+            case "média":
+                precoBase *= 1.5; 
+                break;
+            case "grande":
+                precoBase *= 2; 
+                break;
+
+            default:
+                precoBase*=1;
+                break;
+        }
+        return precoBase;
 }
+    private double definirPrecoBase(String sabor) {
+        double precoBase = precoBaseTamanho(tamanho); 
+        switch (sabor.toLowerCase()) {
+            case "calabresa":
+                precoBase *= 1.1; 
+                break;
+            case "mussarela":
+                precoBase *= 1.1; 
+                break;
+            case "frango com catupiry":
+                precoBase *= 1.3; 
+                break;
+            case "portuguesa":
+                precoBase *= 1.25;
+                break;
+            default:
+                precoBase*=1;
+                break;
+    }
+        return precoBase;
+}
+    }
+    
+
+
+
+
